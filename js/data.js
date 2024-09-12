1beaa063cd22f5526a503ac89f13deb69db82d8a
suraartut <suraartut@gmail.com>
2024-09-13 01:18:33 +0300

    First commit

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const rooms = [
+  {
+    name: "Standart Oda",
+  },
+  {
+    name: "Deluxe Oda",
+  },
+  {
+    name: "Junior Suit",
+  },
+  {
+    name: "Engelli Odası",
+  },
+  {
+    name: "Trible Oda",
+  },
+  {
+    name: "Kral Dairesi",
+  },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        josefin: ["Josefin Sans", "sans-serif"],
+      },
+      boxShadow: {
+        custom:
+          "0px 1px 2px rgba(0, 0, 0, 0.3), 0px 1px 3px 1px rgba(0, 0, 0, 0.15)",
+      },
+    },
+  },
+  plugins: [],
+};
